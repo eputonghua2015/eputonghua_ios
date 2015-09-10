fe1e3c7031bf2ba3ef29bde50803613bc5beb6a9
eputonghua2015 <sdhg@qq.com>
2015-09-10 14:06:39 +0800

    update plist

--- EputonghuaHelloTest/src/com/eputonghuatest/hello/myTest2.java
+++ EputonghuaHelloTest/src/com/eputonghuatest/hello/myTest2.java
@@ -7,7 +7,7 @@ public class myTest2 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.printf("hello eputonghua. Test2 .....");
+		System.out.printf("hello eputonghua. Test2 ......");
 	}
 
 }
