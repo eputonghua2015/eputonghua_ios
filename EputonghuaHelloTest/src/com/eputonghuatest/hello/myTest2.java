6cf422ee286d934681a79b3c0fbe6af555d4ddd3
eputonghua2015 <sdhg@qq.com>
2015-09-10 10:29:43 +0800

    this is test two!

---
+++ EputonghuaHelloTest/src/com/eputonghuatest/hello/myTest2.java
@@ -0,0 +1,13 @@
+package com.eputonghuatest.hello;
+
+public class myTest2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("hello eputonghua. Test2");
+	}
+
+}
